9b6549e8f90b1b814c2ab749eea032d7bed5c71c
Jacob <jake.laurenti@gmail.com>
2017-12-26 17:44:54 -0500

    Added additional comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,26 @@
 (function() {
     function config($locationProvider, $stateProvider) {
+
+      // configure how the application handles URLs in the browser
       $locationProvider
         .html5Mode({
           enabled: true,
           requireBase: false
         });
 
+      // configure the state behavior
       $stateProvider
+        // state name
         .state('home', {
           url: '/',
+          // controller paired to the state using 'controller as' syntax
           controller: 'HomeCtrl as home',
           templateUrl: '/templates/home.html'
         });
     }
     angular
+        // dependencies are injected into the app by adding script source, then adding them to the array below
         .module('pomodoro-2017', ['ui.router', 'firebase'])
+        // makes sure the providers are accessible throughout the application
         .config(config);
 })();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,9 @@
     }
 
     angular
+        // dependencies set in app.js; only need to retrieve the already-defined module
         .module('pomodoro-2017')
+        // inject as many dependencies as our controller in the array below
+        // last item in the array must be the callback function that executes when the controller is initialized
         .controller('HomeCtrl', [HomeCtrl]);
 })();
