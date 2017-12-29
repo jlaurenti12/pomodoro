4fbb422c8a50fcb0b6f311e00b1fca44c0856e19
Jacob <jake.laurenti@gmail.com>
2017-12-28 21:46:01 -0500

    Break Timer working

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -11,23 +11,29 @@
 
       this.timerRunning = false;
 
-  // Start time function initiated when button is clicked
+      this.onBreak = false;
 
-      this.startTimer = function() {
+  // Start time function initiated when 'Start Time / Start a new session' button is clicked
+
+      this.startWorkTimer = function() {
           this.timerRunning = true;
-          this.buttonMsg = "Reset Timer"
+          this.buttonMsg = "Reset Timer";
           this.clock = 9;
           this.timer = $interval(function () {
               this.clock -= 1;
               if (this.clock === 0) {
                 $interval.cancel(this.timer);
+                this.timerRunning = false;
+                this.onBreak = true;
+                this.buttonMsg = "Take a Break - You Earned it!";
+                this.clock = 5;
               }
           }.bind(this), 1000);
       }
 
-  // Reset time function initiated when when timer is running and button is clicked
+  // Reset time function initiated when when work timer is running and 'Reset Timer' button is clicked
 
-      this.resetTimer = function() {
+      this.resetWorkTimer = function() {
           if (angular.isDefined(this.timer)) {
                  $interval.cancel(this.timer);
                  this.clock = 10;
@@ -35,6 +41,36 @@
                  this.buttonMsg = 'Start Timer'
           }
       }
+
+  // Break time function initiated when when work timer gets to 0 and 'Start Break' button is clicked
+
+      this.startBreakTimer = function() {
+          this.timerRunning = true;
+          this.clock = 4;
+          this.buttonMsg = "Reset Break";
+          this.timer = $interval(function () {
+              this.clock -= 1;
+              if(this.clock === 0) {
+                 $interval.cancel(this.timer);
+                 this.timerRunning = false;
+                 this.onBreak = false;
+                 this.buttonMsg = "Start a New Session!"
+                 this.clock = 10;
+              }
+          }.bind(this), 1000);
+       }
+
+   // Reset time function initiated when when break timer is running and 'Reset Break' button is clicked
+
+      this.resetBreakTimer = function() {
+          if (angular.isDefined(this.timer)) {
+              $interval.cancel(this.timer);
+              this.clock = 5;
+              this.timerRunning = false;
+              this.buttonMsg = 'Start Break'
+          }
+       }
+
     }
 
     angular
